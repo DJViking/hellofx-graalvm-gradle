5d4ed89c3602aa05e1e7d463efd747fec6e7556a
Sverre Moe <sverre@spacetec.no>
2022-06-02 14:08:19 +0200

    remove module-info.java

---
+++ src/main/java/hellofx/HelloFXLauncher.java
@@ -0,0 +1,17 @@
+package hellofx;
+
+/**
+ * Launcher for HelloFX JavaFX Application.
+ * <p>
+ * This is necessary for running Non-Modular JavaFX on Java9+.
+ * <p>
+ * Could be removed when application is fully modularized.
+ */
+public class HelloFXLauncher {
+
+    public static void main(String[] args) {
+        HelloFX.main(args);
+    }
+
+}
+
